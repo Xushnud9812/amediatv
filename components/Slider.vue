3f02447cf9f6fc492adfdcdb6ae0f10da42fff5b
Xushnud9812 <hushnud1208@mail.ru>
2021-11-19 11:10:54 +0500

    asd

--- components/Slider.vue
+++ components/Slider.vue
@@ -3,7 +3,9 @@
     <div class="slider">
       <Carousel v-bind="slickOptions">
         <div class="car">
-          <div class="container-slider">
+            <img src="@/assets/img/2.jpg" alt />
+
+          <div class="tex container">
             <div class="text flex">
               <div class="left">
                 <h2>asjgdfa</h2>
@@ -16,7 +18,8 @@
               </div>
             </div>
           </div>
-          <img src="@/assets/img/2.jpg" alt />
+          <div class="img">
+          </div>
         </div>
       </Carousel>
     </div>
@@ -65,50 +68,24 @@ export default {
 
 <style lang="scss" scoped>
 .slider {
-  width: 100%;
-  overflow: hidden;
-  margin-bottom: 30px;
   .car {
-    width: 100%;
+   z-index: 4;
     position: relative;
-    width: 100%;
-    top: 0;
-    left: 0;
-    .container-slider{
-      width: 1140px;
-      margin: 0 auto;
+    overflow: hidden;
+    height: 550px;
+    .tex{
+      z-index: 3;
     }
-    .text {
-
-      z-index: 10;
+    
+    .img {
       position: absolute;
-      top: 50%;
-      transform: translate(0%, -50%);
-      display: flex;
-      .left {
-        width: 200px;
-      }
-      .right {
-        width: 400px;
-        img {
-          width: 100%;
-        }
-      }
-      h2 {
-        font-size: 48px;
-        color: #fff;
-        font-weight: 600;
-        line-height: 130%;
-        margin-bottom: 40px;
-        display: block;
-      }
-    }
-    img {
-      pointer-events: none;
       width: 100%;
       top: 0;
       left: 0;
-      z-index: 0;
+      img {
+        width: 100%;
+        pointer-events: none;
+      }
     }
   }
 }
